7577ff61122182912485ce8833e6fb1f16db5890
Antonio Hickey <antoniohickey99@gmail.com>
2024-07-15 00:55:28 -0400

    feat: implement gelu activation f(x)

--- src/activation.rs
+++ src/activation.rs
@@ -1,4 +1,5 @@
 use crate::Matrix;
+use std::f64::consts::PI;
 use std::ops::{Add, Mul};
 
 impl<T> Matrix<T>
@@ -55,6 +56,31 @@ where
                 } else {
                     T::default()
                 }
+            })
+            .collect();
+
+        Matrix {
+            data,
+            row_size: self.row_size,
+            col_size: self.col_size,
+        }
+    }
+
+    /// Apply the GeLU activation function onto a `Matrix`
+    /// NOTE: Smoother (near 0) than ReLU & potential for regularization effects.
+    pub fn gelu(&self) -> Matrix<T>
+    where
+        T: Copy + PartialOrd + Default + From<f64> + Into<f64>,
+    {
+        let data: Vec<T> = self
+            .data
+            .iter()
+            .map(|&x| {
+                let x_f64: f64 = x.into();
+                let x_gelu = 0.5
+                    * x_f64
+                    * (1.0 + ((2.0 / PI).sqrt() * (x_f64 + 0.04715 * x_f64.powi(3))).tanh());
+                T::from(x_gelu)
             })
             .collect();
 
@@ -125,5 +151,33 @@ mod tests {
         let result = matrix.relu_backward();
 
         assert_eq!(result.data, expected);
+    }
+
+    #[test]
+    /// Verify each element is correctly GeLU'd
+    fn test_gelu() {
+        let matrix = Matrix {
+            data: vec![1.0, -2.0, 3.0, -4.0, 5.0, -6.0, 7.0, -8.0, 9.0],
+            row_size: 3,
+            col_size: 3,
+        };
+
+        let expected = [
+            0.841344746,
+            -0.045500263,
+            2.9963625,
+            -0.0003058,
+            4.9998675,
+            -0.0000009,
+            6.999999998,
+            -0.000000001,
+            8.9999999,
+        ];
+        let result = matrix.gelu();
+
+        for (res, exp) in result.data.iter().zip(expected.iter()) {
+            // NOTE: due to inaccuracy in floating point arithmetic this has a tolerance of 0.01
+            assert!((Into::<f64>::into(*res) - exp).abs() < 1e-2);
+        }
     }
 }
